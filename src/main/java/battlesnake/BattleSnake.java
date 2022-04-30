6d68ba212b73bc7c2cd691a7997f2c6331d7a77f
aschokking <>
2022-04-30 14:50:06 -0700

    Add avoidNeck shell

--- src/main/java/battlesnake/BattleSnake.java
+++ src/main/java/battlesnake/BattleSnake.java
@@ -33,6 +33,7 @@ public class BattleSnake {
         ArrayList<String> possibleMoves = new ArrayList<String>(Arrays.asList("up", "down", "left", "right"));
         
         avoidWalls(board, you, possibleMoves);
+        avoidNeck(you, possibleMoves);
 
         // Choose a random direction to move in
         int choice = (int)(Math.random() * possibleMoves.size());
@@ -41,9 +42,18 @@ public class BattleSnake {
         return move;
     }
 
+    // don't run into walls
     private void avoidWalls(Board board, Snake you, ArrayList<String> possibleMoves) {
         XYPoint head = you.getHead();
 
         // TODO: Remove any directions from possibleMoves that would result in hitting a wall
+    }
+
+    // don't run into our own neck (the part of our body that's before the head)
+    private void avoidNeck(Snake you, ArrayList<String> possibleMoves) {
+        XYPoint head = you.getHead();
+        XYPoint neck = you.getBody()[1];
+
+        // TODO: Remove any directions from possibleMoves that would result in hitting our own neck
     }
 }
